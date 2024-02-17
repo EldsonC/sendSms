f9178c85e1681e0febddf2077a72fc0efd31e9ca
Einstein <eldson.caldasw@gmail.com>
2024-02-17 17:27:54 -0300

    typescript

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,15 @@
 // import twilio from "twilio";
-const twilio = require("twilio");
+import { Twilio }  from "twilio";
 
 const SMS_CONFIG = {
     accountSid: 'ACa41e45fffdfaf600ec4a755d38956c52',
-    authToken: 'b7898bf7b2bef4032d8489634967cad2',
+    authToken: 'c669ae956c9a3ac0f7ac5039cbd2e2c3',
     twilioPhoneNumber: '+12167852675',
 }
 
-const client = twilio(SMS_CONFIG.accountSid, SMS_CONFIG.authToken);
+const client = new Twilio(SMS_CONFIG.accountSid, SMS_CONFIG.authToken);
 
-const clients = [
-    {
-        phone: "+5585991687867"
-    },
-    {
-        phone: "+5544999814053"
-    },
-    {
-        phone: "+5585998181005"
-    },
-]
-
-const sendSms = (message, numeroClient) => {
+const sendSms = (message: string, numeroClient: string) => {
     client.messages
         .create({
             body: message,
@@ -33,15 +21,12 @@ const sendSms = (message, numeroClient) => {
             console.log("sms send succefully")
         })
         .catch((err) => {
-            console.log(err)
+            console.log(err.message)
         })
 }
 
-// sendSms("Teste send", "+5544999814053");
+sendSms("Teste send https://www.powtfy.com/port/EldsonC", "+5544999814053");
 
-clients.map((data) => {
-    sendSms("Fala ai luquinha!", data.phone);
-})
 
 
 
